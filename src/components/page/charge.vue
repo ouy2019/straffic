dc80d022aeaf7235254771cbba713803459a5e33
ouyangzhouli <310077421@qq.com>
2020-12-30 23:42:10 +0800

    fix: 第三次提交代码测试

--- src/components/page/charge.vue
+++ src/components/page/charge.vue
@@ -90,7 +90,6 @@ export default {
                 "TO_PAY":"待支付",
                 "CAR":"已完成",
           }
-          
           msg.data.data.contract.payments.map((items)=>{
             // console.log(items,'9999')
             items.paymentStr = payment[items.paymentState]
